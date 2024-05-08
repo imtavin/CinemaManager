ffd395a15bdf73a12f11d1ac18fd04377a3453ac
Gustavo Espenchitt <92340274+imtavin@users.noreply.github.com>
2024-05-08 20:39:02 -0300

    Add files via upload

---
+++ Cinema/src/br/edu/up/modelos/LastId.java
@@ -0,0 +1,32 @@
+package br.edu.up.modelos;
+
+public class LastId {
+    //Setando o ultimo ID de sessao
+    private static int lastIdSessao = 0;
+    private static int lastIdFilme = 0;
+    private static int lastIdTransacao = 0;
+
+    public static int getLastIdSessao() {
+        return lastIdSessao;
+    }
+
+    public static void setLastIdSessao(int lastIdSessao) {
+        LastId.lastIdSessao = lastIdSessao;
+    }
+
+    public static int getLastIdFilme() {
+        return lastIdFilme;
+    }
+
+    public static void setLastIdFilme(int lastIdFilme) {
+        LastId.lastIdFilme = lastIdFilme;
+    }
+
+    public static int getLastIdTransacao() {
+        return lastIdTransacao;
+    }
+
+    public static void setLastIdTransacao(int lastIdTransacao) {
+        LastId.lastIdTransacao = lastIdTransacao;
+    }
+}
